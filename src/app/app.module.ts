5e48ec9aa9d09cce9244bbc6248cfc23a19ded8a
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-06 02:56:08 -0700

    Add toolbar to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule, MatCheckboxModule,
-  MatToolbarModule,
-  MatSelectModule,
-  MatCardModule,
-  MatIconModule,
-  MatListModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatDividerModule
-}
-  from '@angular/material';
-import { MatStepperModule } from '@angular/material/stepper';
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MyMaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { MealComponent } from './meal/meal.component';
 import { NewMealComponent } from './new-meal/new-meal.component';
-
-import { CompletenessPipe } from './completeness.pipe';
 import { EditMealComponent } from './edit-meal/edit-meal.component';
 import { MealListComponent } from './meal-list/meal-list.component';
+import {ErrorComponent} from './error.component';
+
+import { CompletenessPipe } from './completeness.pipe';
+
 
 import { ToobarMultirowComponent } from './toobar-multirow/toobar-multirow.component';
 import { LoginComponent } from './login/login.component';
@@ -40,21 +31,13 @@ import { LoginComponent } from './login/login.component';
     EditMealComponent,
     MealListComponent,
     ToobarMultirowComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatSelectModule,
-    MatIconModule,
-    MatListModule,
-    
-    MatFormFieldModule,
-    MatInputModule,
+    MyMaterialModule,
     FormsModule,
     ReactiveFormsModule,
     FlexLayoutModule
--- src/app/error.component.ts
+++ src/app/error.component.ts
@@ -2,17 +2,17 @@ import {Component, Inject, Injectable} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
 
 @Component({
-    templateUrl: 'error.compoonent.html'
+  templateUrl: 'error.component.html'
 })
+
 export class ErrorComponent {
-    
-    constructor(private dialogRef: MatDialogRef<ErrorComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
 
-    }
+  constructor(private dialogRef: MatDialogRef<ErrorComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
 
-    public closeDialog() {
-        this.dialogRef.close();
-    }
+  }
 
+  public closeDialog() {
+    this.dialogRef.close();
+  }
 
 }
