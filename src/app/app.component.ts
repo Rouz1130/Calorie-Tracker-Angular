329dce8c6543f8ba6851ca6d9d2773916e82215b
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-05 18:15:18 -0700

    Add template and 3 fucntions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,20 @@ export class AppComponent {
     new Meal("Pizza slice", "foodDescription2", 499),
     new Meal("Bacon & eggs", "foodDescription3", 501),
     new Meal("thai food", "foodDescription4", 502),
-];
+  ];
+
+  selectedMeal: Meal = null;
+
+  ShowDetails(clickedMeal: Meal) {
+    this.selectedMeal = clickedMeal;
+  }
+
+  finishedEditing() {
+    this.selectedMeal = null;
+  }
+
+  addMeal(newMealFromChild: Meal) {
+    this.masterMealList.push(newMealFromChild);
+  }
+
 }
