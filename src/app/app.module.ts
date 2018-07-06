f3eff9021319b210006bb91ea448c7fe086b9946
rouzbeh majlessi <rmajlessi@gmail.com>
2018-07-05 18:00:14 -0700

    Add component pipe skeleton files and add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MealComponent } from './meal/meal.component';
+import { NewMealComponent } from './new-meal/new-meal.component';
+import { NewComponent } from './new/new.component';
+import { CompletenessPipe } from './completeness.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealComponent,
+    NewMealComponent,
+    NewComponent,
+    CompletenessPipe
   ],
   imports: [
     BrowserModule
